42085a8b5e2dc86a3ca7a87e57a0c94d3eaaa089
Eneier <canadacoder94@gmail.com>
2023-08-19 21:41:25 -0400

    create API +  theme

---
+++ OzonBook/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OzonBook.Data;
+using OzonBook.Models;
+
+namespace OzonBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View(new Category());
+        }
+    }
+}
