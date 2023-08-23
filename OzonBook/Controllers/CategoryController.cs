f6540cfa893587c974cd35e7cf519700b14e2934
Eneier <canadacoder94@gmail.com>
2023-08-23 11:24:14 -0400

    Finished CRUD system of Category and added Tostr notifications

--- OzonBook/Controllers/CategoryController.cs
+++ OzonBook/Controllers/CategoryController.cs
@@ -6,24 +6,104 @@ namespace OzonBook.Controllers
 {
     public class CategoryController : Controller
     {
-
         private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
 
-
+        [HttpGet]
         public IActionResult Create()
         {
             return View(new Category());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully"; 
+                TempData["error"] = "Something went wrong"; 
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryId = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (categoryId == null)
+            {
+                return NotFound();
+            }
+            return View(categoryId);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                TempData["error"] = "Something went wrong";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryId = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (categoryId == null)
+            {
+                return NotFound();
+            }
+            return View(categoryId);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.FirstOrDefault(obj => obj.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            TempData["error"] = "Cannot delete category";
+            return RedirectToAction("Index");
         }
     }
 }
--- OzonBook/Models/Category.cs
+++ OzonBook/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OzonBook.Models;
 
@@ -6,7 +7,14 @@ public class Category
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
+    [DisplayName("Category Name")]
+    [MaxLength(30)]
+    [MinLength(3)]
     public string Name { get; set; }
+
+    [DisplayName("Display Order")]
+    [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100")]
     public int DisplayOrder { get; set; }
 }
