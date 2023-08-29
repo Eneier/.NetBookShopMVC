6d486e6dffec085f867303a0525ba73a29edb652
Eneier <canadacoder94@gmail.com>
2023-08-29 10:14:58 -0400

    new API for delete and modal alert for confirmation, added dataTables with sort, pagination, search.

--- OzonBook/Areas/Admin/Controllers/ProductController.cs
+++ OzonBook/Areas/Admin/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace OzonBook.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return View(objProductList);
         }
 
@@ -103,34 +103,41 @@ namespace OzonBook.Areas.Admin.Controllers
             }
         }
 
+        #region API CALLS
+
         [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+
+
+        [HttpDelete]
         public IActionResult Delete(int? id)
         {
-            if (id == null || id == 0)
+            var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productToBeDeleted == null)
             {
-                return NotFound();
+                return Json(new { success = false, message = "Error while deleting" });
             }
-            Product? productId = _unitOfWork.Product.Get(c => c.Id == id);
 
-            if (productId == null)
+            if (!string.IsNullOrEmpty(productToBeDeleted.ImageUrl))
             {
-                return NotFound();
+                string oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('\\'));
+                
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
             }
-            return View(productId);
-        }
 
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? id)
-        {
-            Product? obj = _unitOfWork.Product.Get(obj => obj.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Product.Remove(productToBeDeleted);
             _unitOfWork.Save();
-            TempData["success"] = "Product deleted successfully";
-            return RedirectToAction("Index");
+
+            return Json(new { success = true, message = "Delete Successful" });
         }
+
+        #endregion
     }
 }
